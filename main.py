46cca8f883f6fff881ea60c1280622d4c5cce4ca
hapuhapu0 <tmdtn3624@naver.com>
2024-01-31 23:51:58 +0900

    저장용

--- main.py
+++ main.py
@@ -1,38 +1,20 @@
-from selenium import webdriver
+import requests
 from bs4 import BeautifulSoup
 
-import time
-
-class ChromeDriver:
-    def __init__(self) -> None:
-        self.browser = webdriver.Chrome()
-
-    def getUrl(self, Url: str) -> None:
-        self.browser.get(url=Url)
-
-    def getSourceAsFile(self, Path:str = "SiteSource.txt"):
-        Source = self.browser.page_source
-        with open(Path, "w") as f:
-            f.write(str(Source.encode("utf-8")))
-
-    def getSource(self):
-        self.Source = self.browser.page_source
-
-    def getCompanyName(self):
-        Text = BeautifulSoup(self.Source, 'html.parser')
-        print(Text.select("#content > div.site-wrapper > div > div > div.bsj-template__b > ul.jobs-list-items > li:nth-child(1) > div > div.bjs-jlid__header > div > h4 > a"))
-
+class siteParse:
+    def __init__(self, site: str) -> None:
+        self.headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}
+        self.site = requests.get(site, headers=self.headers)
+        if (self.site.status_code != 200):
+            print(f"status_code error code: {self.site.status_code}")
+        else:
+            print(f"Successful code: {self.site.status_code}")
     
+    def getCompanyName(self) -> list:
+        SiteSource = self.site.text
+        self.ParseSource = BeautifulSoup(SiteSource, "html.parser")
+        print(self.ParseSource.select("#content > div.site-wrapper > div > div > div.bsj-template__b > ul.jobs-list-items > li:nth-child(1) > div > div.bjs-jlid__header > div > h4 > a"))
 
-# 객체생성
-Chrome = ChromeDriver()
 
-# 오픈
-Chrome.getUrl("https://berlinstartupjobs.com/skill-areas/python/")
-
-# 사이트 소스 가져오기
-Chrome.getSource()
-
-Chrome.getCompanyName()
-
-time.sleep(10)
+Site = siteParse("https://berlinstartupjobs.com/engineering/")
+Site.getCompanyName()
